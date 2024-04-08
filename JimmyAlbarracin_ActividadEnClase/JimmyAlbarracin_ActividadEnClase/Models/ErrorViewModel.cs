35af6e9d3b3a550ba38935a5ac02fbbe5c0c31ac
JimmyAlbarrac1n <jimmismael2003@gmail.com>
2024-04-08 10:47:41 -0500

    Agregar archivos de proyecto.

---
+++ JimmyAlbarracin_ActividadEnClase/JimmyAlbarracin_ActividadEnClase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JimmyAlbarracin_ActividadEnClase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ JimmyAlbarracin_Clase/JimmyAlbarracin_Clase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JimmyAlbarracin_Clase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
